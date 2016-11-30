6eb7d90240d3d55dddcbacb8fb60edd1be6addd8
tianyi <mengwang20@qq.com>
2016-11-30 11:08:26 +0800

    1

---
+++ ConsoleTest/ConsoleTest/ConsoleTest.cpp
@@ -0,0 +1,15 @@
+// ConsoleTest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "iostream"
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << 123 << endl;
+	system("PAUSE");
+	return 0;
+}
+
